d04c5e7ce4456e9047155451831029baacdaaa34
Victor Matekole <victor@matekole.com>
2023-08-23 12:32:33 +0200

    parse text func

--- code/transcriber/src/contrario/transcribe.py
+++ code/transcriber/src/contrario/transcribe.py
@@ -1,29 +1,30 @@
+import re
 from abc import ABC
-from typing import Any
+from typing import Union
+
+import pandas as pd
 from injector import inject
-
-from pydub import AudioSegment
-
 from pyannote.audio import Pipeline
+from pydub import AudioSegment
 
 
 class Configuration:
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs: dict[str, Union[str, int]]):
         self._readonly = False
 
         for k, v in kwargs.items():
             setattr(self, k, v)
         self._readonly = True
 
-    def __setattr__(self, __name: str, __v: Any) -> None:
-        if getattr(self, "_readonly", False):
-            raise AttributeError("Transcriber configuration readonly")
+    def __setattr__(self, __name: str, __v) -> None:
+        if getattr(self, '_readonly', False):
+            raise AttributeError('Transcriber configuration readonly')
         super().__setattr__(__name, __v)
 
     def __repr__(self) -> str:
-        state = ""
+        state = ''
         for k, v in self.__dict__.items():
-            state += f"{k}: {v} "
+            state += f'{k}: {v} '
         return state
 
 
@@ -48,23 +49,97 @@ class TranscriberService:
         self._model = model
 
     def transcribe(self, audio_file: str) -> None:
-        self._model.load_audio(audio_file)
-        self._model.transcribe(audio_file)
+        #     self._model.load_audio(audio_file)
+        #     self._model.transcribe(audio_file)
+        pass
 
     def split_audio(self, audio_file: str, seconds: int) -> None:
-        sound = AudioSegment.from_file(audio_file, format="m4a")
+        sound = AudioSegment.from_file(audio_file, format='m4a')
 
         for i, chunk in enumerate(sound[:: seconds * 1000]):
-            with open(f"{audio_file}-{i}.wav", "wb") as f:
-                chunk.export(f, format="wav")
+            with open(f'{audio_file}-{i}.wav', 'wb') as f:
+                chunk.export(f, format='wav')
 
     def diarize(self, audio_file: str):
         pipeline = Pipeline.from_pretrained(
-            "pyannote/speaker-diarization",
-            use_auth_token="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",
+            'pyannote/speaker-diarization',
+            use_auth_token='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',
         )
 
         diarization = pipeline(audio_file)
 
         for turn, _, speaker in diarization.itertracks(yield_label=True):
-            print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
+            print(f'start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}')
+
+    @classmethod
+    def convert_to_milliseconds(cls, time: str) -> int:
+        hours, minutes, seconds = time.split(':')
+        seconds, milliseconds = map(int, seconds.split(','))
+        return (
+            int(hours) * 3600 + int(minutes) * 60 + int(seconds) * 1000
+        ) + milliseconds
+
+    @classmethod
+    def parse_timings(cls, line: str) -> tuple[int, int]:
+
+        if '-->' in line:
+            start_time, end_time = map(str.strip, line.split('-->'))
+
+            return (
+                TranscriberService.convert_to_milliseconds(start_time),
+                TranscriberService.convert_to_milliseconds(end_time),
+            )
+        return 0, 0
+
+    @classmethod
+    def parse_speaker_texts(cls, line: str) -> tuple[str, str]:
+        if re.search(r'SPEAKER_\d+:\s*(.*)', line):
+            speaker, text = map(str.strip, line.split(':'))
+            return speaker, text
+        return '', ''
+
+    def read_srt(self, srt_file: str) -> pd.DataFrame:
+        subtitle_data = []
+
+        with open(srt_file, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+
+            start_end_timings = list(
+                filter(
+                    lambda x: x != (0, 0),
+                    map(TranscriberService.parse_timings, lines),
+                )
+            )
+            speaker_texts = list(
+                filter(
+                    lambda x: x != ('', ''),
+                    map(TranscriberService.parse_speaker_texts, lines),
+                )
+            )
+            subtitle_data = list(zip(start_end_timings, speaker_texts))
+
+            merged_tuple_list = [
+                t_start_end_timings + t_speaker_texts
+                for t_start_end_timings, t_speaker_texts in subtitle_data
+            ]
+
+            return pd.DataFrame(
+                merged_tuple_list, columns=['start_time', 'end_time', 'speaker', 'text']
+            )
+
+    @classmethod
+    def write_transcript(cls, raw_transcripts: pd.DataFrame) -> list[str]:
+
+        lines = []
+        curr_speaker: str = ''
+        for r in raw_transcripts.itertuples(index=False):
+
+            if curr_speaker != r.speaker:
+                lines.append(f'\n\n{r.speaker}\n\n')
+                curr_speaker: str = r.speaker.strip()
+
+            lines.append(f'{r.text} ')
+
+        with open('./temp.txt', mode='w', encoding='utf-8') as file:
+            file.writelines(l for l in lines)
+        return lines
--- code/transcriber/src/main.py
+++ code/transcriber/src/main.py
@@ -7,17 +7,23 @@ from contrario.transcribe import (
 from contrario.transcribers import TranscriberFactory, WhisperXTranscriberModel
 
 from rich import print
+import pandas as pd
 
 
 def configure_transcriber(binder):
     config = Configuration(
-        provider="whisperx", model="medium", device="cpu", compute_type="int8"
+        provider='whisperx', model='medium', device='cpu', compute_type='int8'
     )
     binder.bind(Configuration, to=config, scope=singleton)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     injector: Injector = Injector([configure_transcriber, TranscriberFactory()])
     service: TranscriberService = injector.get(TranscriberService)
     # service.split_audio('/Users/PI/Downloads/joscha_1.m4a', 3600)
-    service.diarize("/Users/PI/Downloads/joscha_1.m4a-0.wav")
+    # service.diarize("/Users/PI/Downloads/joscha_1.m4a-0.wav")
+    df: pd.DataFrame = service.read_srt(
+        '/Users/PI/code/code-from-blog/code/transcriber/diarised_txts/333-andrej-karpathy-tesla-ai-self-driving-optimus-aliens-and-agi-lex-fridman-podcast_DIARY.srt'
+    )
+    print(df.head())
+    text = TranscriberService.write_transcript(df)
